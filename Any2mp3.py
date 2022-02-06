6044bac3067713b23d10f0de61653cf89c8ce4c9
259625 <259625@student.pwr.edu.pl>
2022-02-06 15:06:38 +0100

    ffmpeg for all conversions

--- Any2mp3.py
+++ Any2mp3.py
@@ -1,35 +1,14 @@
 import os
-import sys
-
 
 class Converter:
     def __init__(self, file) -> None:
-        self.file_dir = file
-        self.format = self.file_dir.split("/")[-1]
-        self.song_path = "/Songs/"
 
-    def wmatmp3():
-        pass
-
-    def wav2mp3(self):
         LOG_LEVEL = "fatal"
-        path = sys.argv[0]
-        print(self.file_dir)
-        file_base_name = self.file_dir[:-4]
-        file_wma = os.path.join(self.file_dir, "%s.wma" % file_base_name)
-        file_mp3 = os.path.join(self.song_path, "%s.mp3" % file_base_name)
+        song_name = file.split("/")[-1]
+        song_path = "/home/yanosik2115/Py/Project/Songs/"
+        file_base_name = song_name[:-4]
+        file_mp3 = song_path + file_base_name + ".mp3"
+
         os.system(
-            'ffmpeg -loglevel %s -i "%s" -acodec mp3 "%s"'
-            % (LOG_LEVEL, file_wma, file_mp3)
+            'ffmpeg -loglevel %s -i "%s" -acodec mp3 "%s"' % (LOG_LEVEL, file, file_mp3)
         )
-
-
-# if len(sys.argv) != 2:
-#     sys.exit('Missing input path!')
-
-
-
-# if not os.path.isdir(path):
-#     sys.exit('Incorrect input path!')
-
-# items = os.listdir(path)
